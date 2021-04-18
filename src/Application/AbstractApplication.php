54e3af583f18372ce5690e0ffbe9d3173e405d8c
TheKito <11139894+TheKito@users.noreply.github.com>
2021-04-18 04:02:34 -0300

    Create AbstractApplication.php

---
+++ src/Application/AbstractApplication.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Kito\Application;
+
+/**
+ * Description of AbstractApplication
+ *
+ * @author Instalacion
+ */
+abstract class AbstractApplication
+{
+    public function run()
+    {
+        
+    }
+}
